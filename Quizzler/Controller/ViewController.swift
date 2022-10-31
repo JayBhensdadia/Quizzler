e1cc1cf9b1ce23cfb25affd262d6ec41fdb810cd
Jay Bhensdadia <jayvpatel29@gmail.com>
2022-10-31 19:39:38 +0530

    MVC design

--- Quizzler/Controller/ViewController.swift
+++ Quizzler/Controller/ViewController.swift
@@ -22,44 +22,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var choiceButton3: UIButton!
     
     
-    let quiz = [ ["4+2 = ?","6"], ["4-2 = ?","2"],["4*2 = ?","8"]]
-    var questionNumber = 0
+    var quizBrain = QuizBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        choiceButton1.setTitle("6", for: .normal)
-        choiceButton2.setTitle("2", for: .normal)
-        choiceButton3.setTitle("8", for: .normal)
         updateUI()
     }
     
     
     
     @IBAction func choiceButtonPressed(_ sender: UIButton) {
-        
-        
+       
         let userAnswer = sender.currentTitle!
-        let actualAnswer = quiz[questionNumber][1]
         
-        if userAnswer == actualAnswer {
+        let userGotRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotRight{
             sender.backgroundColor = UIColor.green
-            print("Right")
-        } else {
+        }else{
             sender.backgroundColor = UIColor.red
-            print("Wrong")
         }
         
+        quizBrain.nextQuestion()
         
-        questionNumber = (questionNumber+1)%3
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
-        //updateUI()
+        
+       
     }
     
     @objc func updateUI(){
+        
+        questionLabel.text = quizBrain.getQuestionText()
+        
+        choiceButton1.setTitle(quizBrain.getOption(op: 0), for: .normal)
+        choiceButton2.setTitle(quizBrain.getOption(op: 1), for: .normal)
+        choiceButton3.setTitle(quizBrain.getOption(op: 2), for: .normal)
+        
         choiceButton1.backgroundColor = UIColor.clear
         choiceButton2.backgroundColor = UIColor.clear
         choiceButton3.backgroundColor = UIColor.clear
-        questionLabel.text = quiz[questionNumber][0]
+        
+        progressBar.progress = quizBrain.getProgress()
+        
+        scoreLabel.text = "Score : \(quizBrain.getScore())"
     }
 
 }
--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 * immutability
 
 
+/var/folders/54/124zns7n02d2jg7sr4g6047c0000gn/T/TemporaryItems/NSIRD_screencaptureui_v7zfhK/Screenshot 2022-10-31 at 7.37.18 PM.png
